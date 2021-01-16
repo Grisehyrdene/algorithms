3ccb3c58e4a752f9045c377c2be620034d7695d4
Dmitri <Dmitri.Danshin2017@gmail.com>
2021-01-16 20:25:36 +0300

    binary search

--- 1st.js
+++ 1st.js
@@ -1,1 +1,21 @@
-console.log("Hello world");
+function binarySearch(arr, item){
+   let low = 0;
+   let higt = arr.length - 1;
+   let mid;
+   while (low <= higt){
+        mid = low + Math.round((higt - low)/2);
+        if (item < arr[mid]){
+            higt = mid - 1;
+        }else if(item > arr[mid]){
+            low = mid + 1;
+        }else{
+            return mid;
+        }
+      
+   }
+   return null;
+    
+}
+
+console.log(binarySearch([1,3,5,7,9, 10], 10));
+
